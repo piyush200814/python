6a2ffbddd9cd6c4732eea050cbbf792397855efa
piyush200814 <piyushupadhaya32@gmail.com>
2026-01-04 19:21:49 +0530

    Arithmetic operation in python

---
+++ Arithmetic.py
@@ -0,0 +1,11 @@
+#Arithmetic operations in python
+a=int(input("a="))
+b=int(input("b="))
+c=a+b
+d=a-b
+e=a*b
+f=a/b
+print("add=",c) #addition of a and b
+print("sub=",d) #subtraction of a and b
+print("mul=",e) #multiplication of a and b
+print("div=",f) #division of a and b
